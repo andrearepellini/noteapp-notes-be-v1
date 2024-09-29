1d74781ec07b54aa8f5c0fb04458e2efd24b7f42
Andrea Repellini <repellini.andrea@gmail.com>
2024-09-29 15:35:49 +0200

    setup application

---
+++ src/main/java/com/example/notes_be_v1/NotesBeV1Application.java
@@ -0,0 +1,13 @@
+package com.example.notes_be_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesBeV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesBeV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/notes_be_v1/NotesBeV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notes_be_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesBeV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
